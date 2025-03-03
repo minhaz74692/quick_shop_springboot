d543fdb6792bbcad0656affab8d8481e0940edc3
minhaz74692 <minhaz.ipe.sust@gmail.com>
2025-03-04 02:49:09 +0600

    Changes

--- src/main/java/com/mie/quickshop/controller/security/SecurityController.java
+++ src/main/java/com/mie/quickshop/controller/security/SecurityController.java
@@ -1,7 +1,9 @@
 package com.mie.quickshop.controller.product;
 
+import com.mie.quickshop.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +17,10 @@ public class SecurityController {
         return  httpServletRequest.getSession().getId();
     }
 
+    // if you want to check the csrf-token then disable the  httpSecurity.csrf(AbstractHttpConfigurer::disable) code from SecurityConfiguration.java file
     @GetMapping("/csrf-token")
-    public CsrfToken getCSRF(HttpServletRequest httpServletRequest){
-        return (CsrfToken) httpServletRequest.getAttribute("_csrf");
+    public ResponseEntity<ApiResponse> getCSRF(HttpServletRequest httpServletRequest){
+        CsrfToken token = (CsrfToken) httpServletRequest.getAttribute("_csrf");
+        return ResponseEntity.ok(new ApiResponse("Token", token));
     }
 }
