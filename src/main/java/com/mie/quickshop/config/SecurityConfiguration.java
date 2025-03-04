413d6ebe092a0b9f6122c2ede82577509c8dbbf3
minhaz74692 <minhaz.ipe.sust@gmail.com>
2025-03-04 17:17:58 +0600

    Added Basic auth with InMemoryUserDetailsManager

--- src/main/java/com/mie/quickshop/config/SecurityConfiguration.java
+++ src/main/java/com/mie/quickshop/config/SecurityConfiguration.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,7 +24,31 @@ public class SecurityConfiguration {
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(Customizer->Customizer
                         .requestMatchers(api+ "/security/*").permitAll()
+//                        .requestMatchers(api + "/product/**").permitAll() // Add this line
+
                         .anyRequest().authenticated());
         return  httpSecurity.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.builder()
+                .username("minhaz")
+                .password(bCryptPasswordEncoder().encode("12345"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("monir")
+                .password(bCryptPasswordEncoder().encode("12345"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
 }
